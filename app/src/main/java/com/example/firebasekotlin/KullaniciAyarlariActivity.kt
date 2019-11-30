9f9d610438068b18c1caf48f3a16487c8ac05c8d
birincioglu1 <30694561+birincioglu1@users.noreply.github.com>
2019-11-30 19:20:41 +0300

    hesap ayarlari

--- app/src/main/java/com/example/firebasekotlin/KullaniciAyarlariActivity.kt
+++ app/src/main/java/com/example/firebasekotlin/KullaniciAyarlariActivity.kt
@@ -2,7 +2,9 @@ package com.example.firebasekotlin
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_kullanici_ayarlari.*
@@ -15,7 +17,7 @@ class KullaniciAyarlariActivity : AppCompatActivity() {
         var kullanici=FirebaseAuth.getInstance().currentUser!!
 
         etDetayName.setText(kullanici.displayName.toString())
-        etDetayMail.setText(kullanici.email.toString())
+        etDetaySifre.setText(kullanici.email.toString())
 
         btnSifreGonder.setOnClickListener {
             FirebaseAuth.getInstance().sendPasswordResetEmail(kullanici.email.toString())
@@ -31,7 +33,7 @@ class KullaniciAyarlariActivity : AppCompatActivity() {
                 }
         }
         btnDegisikleriKaydet.setOnClickListener {
-            if(etDetayName.text.toString().isNotEmpty()&&etDetayMail.text.toString().isNotEmpty()){
+            if(etDetayName.text.toString().isNotEmpty()&&etDetaySifre.text.toString().isNotEmpty()){
                 if(!etDetayName.text.toString().equals(kullanici.email.toString()))
                 {
                       var bilgilerGuncelle= UserProfileChangeRequest.Builder()
@@ -44,6 +46,7 @@ class KullaniciAyarlariActivity : AppCompatActivity() {
                             }
                         }
 
+
                 }
 
             }else{
@@ -51,6 +54,27 @@ class KullaniciAyarlariActivity : AppCompatActivity() {
                 Toast.makeText(this@KullaniciAyarlariActivity,"Boş alanlari Doldurunuz",Toast.LENGTH_SHORT).show()
 
 
+            }
+        }
+
+        btnSifreveyaMailGuncelle.setOnClickListener {
+            if(etDetaySifre.text.toString().isNotEmpty())
+            {
+                var credential=EmailAuthProvider.getCredential(kullanici.email.toString(),etDetaySifre.text
+                    .toString())
+                kullanici.reauthenticate(credential)
+                    .addOnCompleteListener{task ->
+                        if(task.isSuccessful)
+                        {
+                            guncelleLayout.visibility=View.VISIBLE
+
+                        }else
+                        {
+                            Toast.makeText(this@KullaniciAyarlariActivity,"Şuanki şifreniz yanlış girdiniz",Toast.LENGTH_SHORT).show()
+                        }
+                    }
+            }else{
+                Toast.makeText(this@KullaniciAyarlariActivity,"Güncellemeler için geçerli şifrenizi yazmalısınız",Toast.LENGTH_SHORT).show()
             }
         }
     }
